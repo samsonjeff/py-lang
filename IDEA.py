b329de229268df2896bad08bcc4b94ec3773b842
blurdev <159640757+samsonjeff@users.noreply.github.com>
2025-08-31 09:25:11 +0800

    Delete IDEA.py

--- IDEA.py
+++
@@ -1,6 +0,0 @@
-#creat an laudry app
-#where app can use to pick up and drop cloths to the laundry shop bact to the customer
-
-#create an website that users will able to choose AI chatbot
-#problem to solve is: to easily change ai chatbot in personal purposes
-

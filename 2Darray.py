e9d242c8be68e75623676cb4b947c83b52bfe184
samsonjeff <jeffersonsamson380@gmail.com>
2026-04-11 00:58:44 +0800

    update 2darray.py

--- 2Darray.py
+++ 2Darray.py
@@ -5,18 +5,31 @@ import numpy as np
 np_2d = np.array([[1,2,3,4,5],
                   [6,7,8,9,0]])
 
-string_2d = np.array([[1,2,3,4,5],
-                  ["6",7,8,9,0]]) #convert 1 and all will become string
+string_2d = np.array([[2,1,3,5,4],
+                  ["6",8,0,9,7]]) #convert 1 and all will become string
 
 a = string_2d[1][0]
 b = type(a)
 c = string_2d.dtype
 d = string_2d.shape
+e = string_2d.ndim
+f = np.median(string_2d.astype(float)) #find median and convert it into float
+g = np.sort(string_2d[:,:].astype(int)) # sort each row
+
 
 print(f"print string data {a},type of data is {b}, data inside of string_2d {c}, shape of string_2d is {d} ")
 
 #subsetting
 print(string_2d[ : , 1:3])
-#output     # [['2' '3']
+#>          # [['2' '3']
             #  ['7' '8']]
+
 print(string_2d[1, : ])
+#> ['6' '7' '8' '9' '0']
+
+# check the count of dimention to determine
+print(e)
+
+print(f"display median {f}")
+
+print(f"display sort {g}")

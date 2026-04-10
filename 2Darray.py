2e09df5deb960a690f34ad56ec570a0aee619a4d
samsonjeff <jeffersonsamson380@gmail.com>
2026-04-10 17:15:52 +0800

    add 2Darray.py

--- 2Darray.py
+++ 2Darray.py
@@ -8,6 +8,9 @@ np_2d = np.array([[1,2,3,4,5],
 string_2d = np.array([[1,2,3,4,5],
                   ["6",7,8,9,0]]) #convert 1 and all will become string
 
-print("type of" , type(string_2d))
-print("data inside" , string_2d.dtype)
-print("array shape" , string_2d.shape)
+a = string_2d[1][0]
+b = type(a)
+c = string_2d.dtype
+d = string_2d.shape
+
+print(f"print string data {a},type of data is {b}, data inside of string_2d {c}, shape of string_2d is {d} ")
